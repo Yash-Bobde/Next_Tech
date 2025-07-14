a5b85521792b84b9f23a3216cb620a44f9ee6999
Yash Bobde <ymbobde99@gmail.com>
2025-07-14 13:25:46 -0400

    Add metadata for About page with title, description, and keywords

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+    title: 'About Yash',
+    description: 'This is the about page of Yash',
+    keywords: 'about, yash, home',
+};
 export default function About(){
     return <h1>About my Home!!</h1>;
 }
